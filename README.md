852a85d665bd285c14701da3f4af4e52c354b3fe
abrasax <whoismath@gmail.com>
2021-03-09 02:13:16 -0300

    Initial server setup + start to implement game session

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 I'm trying to develop an online version of coup using python, and in the worst scenario, this will probably work
 
 # Para de ficar olhando ai e vem ajudar meo
+Stalkers are not allowed
+
+# TODO
+- [X] task 01: Develop initial room management system
+- [ ] task 02: Implement session to separete different players
+- [ ] task 03: Implement the entire game logic
+- [ ] task 04: Make things work better and fix bugs
+- [ ] task 05: Make management system delete old rooms (i'm not in the mood to do this yet)
+- [ ] task 06: Make some info be sincronized with the server, like players
+- [ ] task 07: Make friends to play this game with me (most difficult step)
--- server.py
+++ server.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session
 import uuid
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+class Player:
+    """
+    This class is responsible for help storing player information.
+    """
+    # Player info
+    uuid = None
+    room = None
+    religion = None
+    first_card = None
+    second_card = None
+    coins = None
+
+    def __init__(self, room, religion, first_card, second_card, coins):
+        """Game player constructor"""
+        self.uuid = str(uuid.uuid4())
+        self.room = room
+        self.religion = religion
+        self.first_card = first_card
+        self.second_card = second_card
+        self.coins = coins
+
+    def get_uuid(self):
+        """Get player uuid"""
+        return str(self.uuid)
 
 class Game:
     """
@@ -9,19 +36,31 @@ class Game:
 
     Please read https://www.ultraboardgames.com/coup/reformation.php for more info.
     """
-    # Game identifier
+    # Game info
     uuid = None
     players = 0
 
     # Game cards
+    duke = 6
+    assassin = 6
+    countess = 6
+    captain = 6
+    ambassador = 6
+    inquisitor= 6
 
 
     def __init__(self, players):
+        """Game room constructor"""
         self.uuid = str(uuid.uuid4())
         self.players = players
 
     def get_uuid(self):
+        """Get room uuid"""
         return str(self.uuid)
+
+    def add_player(self):
+        """Add players to the game"""
+        return
     
 
 class Server:
@@ -52,17 +91,21 @@ class Server:
         """Create new game for the given number of players, each game is a room"""
         players = int(players)
         game = Game(players)
-        self.running_games[game.get_uuid()] = True
+        self.running_games[game.get_uuid()] = game
         return game.get_uuid()
     
     def get_room(self, room):
         """Get a game context information (a room)"""
         if room not in self.running_games:
             return None
+            
         return True
 
-# Start the server
-game_server = None
+    def init_player(self, room):
+        """Add a new player to the game"""
+        self.running_games[room].add_player()
+
+        # lalalala add session variables
 
 # This is the main route and must be used to create a new game
 @app.route('/', methods=['GET'])
@@ -90,15 +133,18 @@ def game_room(uuid):
     global game_server
     if game_server.get_room(uuid) == None:
         return render_template("error.html", error_message="Seems that this room doesn't exists!")
+    
+    if session.get('room') == None:
+        session['room'] = uuid
+        game_server.init_player(uuid)
 
     return render_template("room.html")
 
 # Setup the game
 if __name__ == '__main__':
+    # Start the server
     global game_server
-
-    # Start the game server
-    global_server = Server()
+    game_server = Server()
 
     # Start the web server
     app.run(port=5000, debug=True)
